fbb86df0a058643ee8b9181d8845b14347d9009e
Ankit Bose <abinfo2310@gmail.com>
2024-11-17 15:28:52 +0530

    Authentication Service using only defined username password from application properties.

--- src/main/java/com/springboot/university/security/SecurityConfig.java
+++ src/main/java/com/springboot/university/security/SecurityConfig.java
@@ -2,8 +2,11 @@ package com.springboot.university.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 /*
@@ -16,6 +19,18 @@ APIs from controller without any username and password.
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        //disables CSRF - Since we disable the CSRF, we do not need to send X-CSRF-TOKEN for POST Request.
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        //Adding these two statements will show forbidden access as
+        // we need to add formLogin() method for UI login and httpBasic() method for REST API login
+        http.authorizeHttpRequests(customizer -> customizer.anyRequest().authenticated());
+        http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
+
+        //This does not maintains any state of current connection and creates new sessioon id every time user logsin.
+        //If this statement is added, we cannot use formLogin(), it will keep redirecting us in the formLogin only.
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
     }
 }
